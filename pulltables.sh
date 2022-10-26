9b44166fb5014fe98f0110d2f3c736586c87f922
Andrea Jegher <andrea.jegher@securenetwork.it>
2022-10-26 17:14:53 +0200

    Added checks to pull tables scripts

--- pulltables.sh
+++ pulltables.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+if [ ! -s "./.env" ]; then
+    echo "No env file"
+    exit 1
+fi
+
+source .env
+
+if [ ! $(which usql) ]; then
+    echo "usql bin not found"
+    exit 2
+fi
+
 while read table
 do
+    echo Downloading "$table"
     usql -q oracle://$USR:$PWD@$HST:1540/$SID --csv -c "SELECT * FROM $table" > ./site/tables/$table.csv
 done < tables.txt
