d8a8f0cda0aa8327f4575aeda17446b60ec5b691
Andrea Jegher <andrea.jegher@securenetwork.it>
2023-02-06 17:54:48 +0100

    improved simple go server

--- main.go
+++ main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
 	serveDirPath := flag.String("d", "./site", "Full or relative path of directory to serve")
+	port := flag.Int("p", 9090, "Server port")
 	flag.Parse()
 
 	http.Handle("/", http.FileServer(http.Dir(*serveDirPath)))
-	port := ":9090"
-	log.Fatal(http.ListenAndServe(port, nil))
+
+	fmt.Printf("Listening on http://127.0.0.1:%d\n", *port)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
